5a454be9027cd549577516fbd0c6a415feea5982
Damith <damithme@gmail.com>
2017-09-17 19:00:50 +0530

    initial commit

---
+++ src/main/java/com/mydevgeek/UserController.java
@@ -0,0 +1,101 @@
+package com.mydevgeek;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Damith Ganegoda on 9/3/17.
+ */
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	private List<User> users;
+
+
+	UserController() {
+		this.users = buildUsers();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> getUsers() {
+		if (this.users.size() == 0) {
+			return Collections.emptyList();
+		} else {
+			return this.users;
+		}
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public User getUser(@PathVariable("id") Long id) {
+		return this.users.stream().filter(user -> user.getId() == id).findFirst().get();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public User saveUser(@RequestBody User user) {
+		Long nextId = 0L;
+		if (this.users.size() != 0) {
+			User lastUser = this.users.stream().skip(this.users.size() - 1).findFirst().get();
+			nextId = lastUser.getId() + 1;
+		}
+
+		user.setId(nextId);
+		this.users.add(user);
+		return user;
+
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public User updateUser(@RequestBody User user) {
+		User modifiedUser = this.users.stream().filter(u -> u.getId() == user.getId()).findFirst().get();
+		modifiedUser.setFirstName(user.getFirstName());
+		modifiedUser.setLastName(user.getLastName());
+		modifiedUser.setEmail(user.getEmail());
+		return modifiedUser;
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public boolean deleteUser(@PathVariable Long id) {
+		User deleteUser = this.users.stream().filter(user -> user.getId() == id).findFirst().get();
+		if (deleteUser != null) {
+			this.users.remove(deleteUser);
+			return true;
+		} else  {
+			return false;
+		}
+
+
+	}
+
+	List<User> buildUsers() {
+		List<User> users = new ArrayList<>();
+
+		User user1 = buildUser(1L, "John", "Doe", "john@email.com");
+		User user2 = buildUser(2L, "Jon", "Smith", "smith@email.com");
+		User user3 = buildUser(3L, "Will", "Craig", "will@email.com");
+		User user4 = buildUser(4L, "Sam", "Lernorad", "sam@email.com");
+		User user5 = buildUser(5L, "Ross", "Doe", "ross@email.com");
+
+		users.add(user1);
+		users.add(user2);
+		users.add(user3);
+		users.add(user4);
+		users.add(user5);
+
+		return users;
+
+	}
+
+	User buildUser(Long id, String fname, String lname, String email) {
+		User user = new User();
+		user.setId(id);
+		user.setFirstName(fname);
+		user.setLastName(lname);
+		user.setEmail(email);
+		return user;
+	}
+
+}
