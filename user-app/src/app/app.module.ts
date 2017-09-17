16cc923ed323eebe30bb2cdb81f43cff01cfa93f
Damith <damithme@gmail.com>
2017-09-17 21:21:25 +0530

    part 1 complete

--- user-app/src/app/app.module.ts
+++ user-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {UserModule} from "./user/user.module";
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
